a34153be93a6122733e48a42e50517a689d78c53
Delmat237 <azangueleonel9@gmail.com>
2025-09-02 22:45:19 +0100

    Suppression de la dependances au .env

--- src/main/java/com/customworld/CustomWorldBackendApplication.java
+++ src/main/java/com/customworld/CustomWorldBackendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import com.customworld.config.FileStorageConfig;
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 
 /**
  * Classe principale de l'application CustomWorld
@@ -14,10 +14,10 @@ import io.github.cdimascio.dotenv.Dotenv;
 @EnableConfigurationProperties(FileStorageConfig.class)
 public class CustomWorldBackendApplication {
     public static void main(String[] args) {
-         Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(entry -> {
-            System.setProperty(entry.getKey(), entry.getValue());
-        });
+        //  Dotenv dotenv = Dotenv.load();
+        // dotenv.entries().forEach(entry -> {
+        //     System.setProperty(entry.getKey(), entry.getValue());
+        // });
         SpringApplication.run(CustomWorldBackendApplication.class, args);
     }
 }
