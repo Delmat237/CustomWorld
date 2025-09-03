85fe705eccff32fb9abc3d43f3ebc3aabd0c824d
Delmat237 <azangueleonel9@gmail.com>
2025-09-03 12:25:13 +0100

    Authorisation des la recuperation du contexte et produit par tous

--- src/main/java/com/customworld/config/SecurityConfig.java
+++ src/main/java/com/customworld/config/SecurityConfig.java
@@ -53,8 +53,8 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Autoriser toutes les requêtes OPTIONS
                 .requestMatchers(AUTH_WHITELIST).permitAll()
-                .requestMatchers("/api/payments/notify").permitAll() // Autoriser explicitement /api/payments/notify
-                .requestMatchers("/api/customer/**").hasAnyRole("CUSTOMER", "ADMIN")
+                .requestMatchers("/api/payments/notify","/api/customer/products","/api/customer/context","/api/customer/categories","/api/customer/products/by-category").permitAll() // Autoriser explicitement /api/payments/notify
+                .requestMatchers("/api/customer/cart/**","/api/customer/orders/**").hasAnyRole("CUSTOMER", "ADMIN")
                 .requestMatchers("/api/vendor/**").hasAnyRole("VENDOR", "ADMIN")
                 .requestMatchers("/api/delivery/**").hasAnyRole("DELIVERER", "ADMIN")
                 .requestMatchers("/api/admin/**").hasRole("ADMIN")
