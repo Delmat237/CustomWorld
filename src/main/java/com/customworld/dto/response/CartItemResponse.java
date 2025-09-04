3362817eb57945d2208f3388490b17e9c714473b
Delmat Leonel AZANGUE <142000059+Delmat237@users.noreply.github.com>
2025-09-04 12:00:20 +0100

    Update CartItemResponse.java

--- src/main/java/com/customworld/dto/response/CartItemResponse.java
+++ src/main/java/com/customworld/dto/response/CartItemResponse.java
@@ -12,5 +12,5 @@ public class CartItemResponse {
     private String productName;
     private Double price;
     private int quantity;
-    private string imageURL;
+    private string imagePath;
 }
